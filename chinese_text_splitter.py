cef44c837b32ac3919c232845d7298caa1b2568e
whitelangpro <33127756+whitelangpro@users.noreply.github.com>
2023-08-15 23:06:53 +0800

    Add files via upload

--- chinese_text_splitter.py
+++ chinese_text_splitter.py
@@ -10,16 +10,24 @@ class ChineseTextSplitter(CharacterTextSplitter):
         self.pdf = pdf
 
     def split_text(self, text: str) -> List[str]:
+        pre_txt = ""
         if self.pdf:
             text = re.sub(r"\n{3,}", "\n", text)
             text = re.sub('\s', ' ', text)
             text = text.replace("\n\n", "。")
         sent_sep_pattern = re.compile(
-            '([﹒﹔﹖﹗．。！？]["’”」』]{0,2}|(?=["‘“「『]{1,2}|$))') 
+            '([■●•﹒﹔﹖﹗．。！？]|\d{1,3}-\d{1,3}\.|$)') 
+        pre_sep_pattern = re.compile('([■●•]|\d{1,3}-\d{1,3}\.)') 
+        stop_pattern = re.compile('.*→P\..*')
         sent_list = []
         for ele in sent_sep_pattern.split(text):
-            if sent_sep_pattern.match(ele) and sent_list:
+            if stop_pattern.match(ele) :
+                pre_txt = ""
+            elif pre_sep_pattern.match(ele) :
+                pre_txt = ele
+            elif sent_sep_pattern.match(ele) and sent_list:
                 sent_list[-1] += ele
             elif ele:
-                sent_list.append(ele)
+                sent_list.append(re.sub("[■●•]", "", pre_txt+ele))
+                pre_txt = ""
         return sent_list
